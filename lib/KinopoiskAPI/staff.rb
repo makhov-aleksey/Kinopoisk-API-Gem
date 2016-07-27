e8752c651e14f60c49112d4b485bdbce647adff2
proFox <profox.rus@gmail.com>
2016-07-27 20:05:25 +0300

    Version 0.0.16

--- lib/KinopoiskAPI/staff.rb
+++ lib/KinopoiskAPI/staff.rb
@@ -8,11 +8,17 @@ module KinopoiskAPI
       @json = json
     end
 
-    # def get_json
-    #   @json
-    # end
+    def get_json
+      @json
+    end
 
     def all
+      {
+
+      }
+    end
+
+    def correctly
       correctly = {}
       creators.each do |items|
         items.each do |item|
@@ -21,10 +27,9 @@ module KinopoiskAPI
       end
     end
 
-    def profession(name)
-      all[name]
-    end
-
+    # def url
+    #   @json['webURL']
+    # end
 
     private
 
