c3de61d1ab3ec4bb55f220d75570c3f5b6ec3f94
proFox <profox.rus@gmail.com>
2016-07-27 19:00:48 +0300

    Version 0.0.3

--- lib/KinopoiskAPI/film.rb
+++ lib/KinopoiskAPI/film.rb
@@ -15,6 +15,10 @@ module KinopoiskAPI
       JSON.parse(response)
     end
 
+    def url
+      @json['webURL']
+    end
+
     def title
       @json['nameRU']
     end
@@ -35,12 +39,48 @@ module KinopoiskAPI
       @json['year']
     end
 
-    def kinopoisk_rating
-      @json['ratingData']['rating']
+    def kinopoisk
+      {
+          rating: @json['ratingData']['rating'],
+          quantity: @json['ratingData']['ratingVoteCount'].delete(' '),
+
+          good_reviews_in_percentage: @json['ratingData']['ratingGoodReview'],
+          number_of_good_reviews: @json['ratingData']['ratingGoodReviewVoteCount'],
+
+          waiting_in_percentage: @json['ratingData']['ratingAwait'],
+          number_of_waiting: @json['ratingData']['ratingAwaitCount'].delete(' '),
+      }
     end
 
-    def imdb_rating
-      @json['ratingData']['ratingIMDb']
+    def imdb
+      {
+          rating: @json['ratingData']['ratingIMDb'],
+          quantity: @json['ratingData']['ratingIMDbVoteCount'].delete(' ')
+      }
+    end
+
+    def number_of_reviews
+      @json['reviewsCount']
+    end
+
+    def duration
+      @json['filmLength']
+    end
+
+    def countries
+      @json['country']
+    end
+
+    def genres
+      @json['genre']
+    end
+
+    def video
+      @json['videoURL']
+    end
+
+    def minimal_age
+      @json['ratingAgeLimits']
     end
 
   end
