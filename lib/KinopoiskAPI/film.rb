e884415e2a57fab225ef0a616215adb5cadba5e2
proFox <profox.rus@gmail.com>
2016-08-14 15:22:45 +0300

    Version 0.6.9

--- lib/KinopoiskAPI/film.rb
+++ lib/KinopoiskAPI/film.rb
@@ -225,7 +225,7 @@ module KinopoiskAPI
     end
 
     def creators
-      @json['creators'].present? ? @json['creators'] : nil
+      @json['creators'].nil? ? nil : @json['creators']
     end
 
   end
--- lib/KinopoiskAPI/staff.rb
+++ lib/KinopoiskAPI/staff.rb
@@ -55,7 +55,7 @@ module KinopoiskAPI
     end
 
     def creators
-      @json['creators'].present? ? @json['creators'] : nil
+      @json['creators'].nil? ? nil : @json['creators']
     end
 
   end
